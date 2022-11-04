45774d38ec626d53daabed06428dc0a007fffa5a
Ananda Kelvin Power Situmorang <110171713+vinmora@users.noreply.github.com>
2022-11-04 20:14:41 +0700

    gas ampe kelar 5 soal

---
+++ soal5.pas
@@ -0,0 +1,37 @@
+program soal5;
+
+var
+j,k,l : integer;
+
+
+begin
+writeln;
+writeln;
+writeln;
+
+    j:=0;
+    while j < 10 do
+begin
+    j:=j+1;
+    k:=0;
+    while k < 10 do
+begin
+    k:=k+1;
+    l:=j * k;
+    write(j,' x ',k,'  = ');
+    if (l mod 4 = 0) then
+        begin 
+        end
+    else if (l mod 6 = 0) then
+        begin 
+        end
+    else 
+    begin
+    write(l:3);
+    end;
+    writeln;
+end;
+    writeln;
+end;
+    readln;
+    end.
