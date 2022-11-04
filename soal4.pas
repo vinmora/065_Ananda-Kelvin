a089b069de62586bb32c14a025360d846edf2fd8
Ananda Kelvin Power Situmorang <110171713+vinmora@users.noreply.github.com>
2022-11-04 19:16:20 +0700

    upload soal 4

---
+++ soal4.pas
@@ -0,0 +1,26 @@
+program soal4;
+
+uses crt;
+var 
+i : integer;
+
+
+begin
+    clrscr;
+
+    for i := 1 to 20 do 
+        begin 
+            write(i); write(' : ');
+
+            if (i mod 2 = 1) then 
+                begin 
+                    write('ganjil');
+                    end
+            else 
+                begin 
+                write('genap');
+                end;
+
+            writeln;
+        end;
+end.
