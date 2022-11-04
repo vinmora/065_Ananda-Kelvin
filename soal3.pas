2f6ad802d8d71c67af244080d69a4584486f4ac6
Ananda Kelvin Power Situmorang <110171713+vinmora@users.noreply.github.com>
2022-11-04 18:58:20 +0700

    Add files via upload

--- soal3.pas
+++ soal3.pas
@@ -1,51 +1,26 @@
-program pasp;
+program soal4;
 
 uses crt;
-
 var 
-i,j,k,l : integer;
+i : integer;
 
 
 begin
-clrscr;
+    clrscr;
 
-   for i := 1 to 4 do
-   begin
-            for j := 4-1 downto i do
-                begin
-                write('  ');
+    for i := 1 to 20 do 
+        begin 
+            write(i); write(' : ');
+
+            if (i mod 2 = 1) then 
+                begin 
+                    write('ganjil');
+                    end
+            else 
+                begin 
+                write('genap');
                 end;
-             
-            for j := 1 to i-1 do
-                begin
-                write('* ');
-                end;
-            for j := 1 to i do
-                begin
-                write('* ');
-                end;
-
-
-             writeln;          
-    end;
- for k := 4 downto 1 do
-    begin 
-         for l:= 4-1 downto k-1 do
-            begin
-            write('  ');
-            end;
-        for l:= 1 to k-1 do
-            begin
-            write('* ');
-            end;
-
-        for l:= 1 to k-2 do
-            begin 
-            write('* ');
-            end;
-
-
 
             writeln;
-    end;
+        end;
 end.
