4acc6d9990ef09e52b73fe4f6f3b0669e40c083b
Ananda Kelvin Power Situmorang <110171713+vinmora@users.noreply.github.com>
2022-11-07 22:40:57 +0700

    clear 5 soal

--- soal5.pas
+++ soal5.pas
@@ -19,10 +19,7 @@ begin
     k:=k+1;
     l:=j * k;
     write(j,' x ',k,'  = ');
-    if (l mod 4 = 0) then
-        begin 
-        end
-    else if (l mod 6 = 0) then
+    if (l mod 4 = 0) and (l mod 6 = 0) then
         begin 
         end
     else 
